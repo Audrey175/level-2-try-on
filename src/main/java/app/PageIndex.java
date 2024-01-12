3ccdcdae7e8492c535506a30a7ff43579015beae
zyra-thant <142961184+zyra-thant@users.noreply.github.com>
2024-01-12 20:01:58 +0700

    new file:   .devcontainer/Dockerfile new file:   .devcontainer/devcontainer.json new file:   .gitignore modified:   README.md new file:   pom.xml deleted:    resources/css/LGA.css new file:   src/app/App.java modified:   src/main/java/app/JDBCConnection.java deleted:    src/main/java/app/LGA.java deleted:    src/main/java/app/LGAhtml.java deleted:    src/main/java/app/Level2A.java new file:   src/main/java/app/Movie.java new file:   src/main/java/app/PageIndex.java new file:   src/main/resources/css/common.css new file:   src/main/resources/images/logo.png

--- src/main/java/app/PageIndex.java
+++ src/main/java/app/PageIndex.java
@@ -2,46 +2,149 @@ package app;
 
 import java.util.ArrayList;
 
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 /**
- * Class for Managing the JDBC Connection to a SQLLite Database.
- * Allows SQL queries to be used with the SQLLite Databse in Java.
+ * Example Index HTML class using Javalin
+ * <p>
+ * Generate a static HTML page using Javalin
+ * by writing the raw HTML into a Java String object
  *
  * @author Timothy Wiley, 2023. email: timothy.wiley@rmit.edu.au
  * @author Santha Sumanasekara, 2021. email: santha.sumanasekara@rmit.edu.au
  */
-public class JDBCConnection {
+public class PageIndex implements Handler {
 
-    // Name of database file (contained in database folder)
-    public static final String DATABASE = "jdbc:sqlite:database/Movies.db";
+    // URL of this page relative to http://localhost:7001/
+    public static final String URL = "/";
 
-    /**
-     * This creates a JDBC Object so we can keep talking to the database
-     */
-    public JDBCConnection() {
-        System.out.println("Created JDBC Connection Object");
+    @Override
+    public void handle(Context context) throws Exception {
+        // Create a simple HTML webpage in a String
+        String html = "<html>";
+
+        // Add some Head information
+        html = html + "<head>" + 
+               "<title>All Movies</title>";
+
+        // Add some CSS (external file)
+        html = html + "<link rel='stylesheet' type='text/css' href='common.css' />";
+        html = html + "</head>";
+
+        // Add the body
+        html = html + "<body>";
+
+        // Add the topnav
+        // This uses a Java v15+ Text Block
+        html = html + """
+            <div class='topnav'>
+                <a href='/'>Homepage</a>
+            </div>
+        """;
+
+        // Add header content block
+        html = html + """
+            <div class='header'>
+                <h1>
+                    <img src='logo.png' class='top-image' alt='RMIT logo' height='75'>
+                    Week 05 exercise index.html page
+                </h1>
+            </div>
+        """;
+
+        // Add Div for page Content
+        html = html + "<div class='content'>";
+
+        // Next we will ask this *class* for the movies
+        ArrayList<String> movieTitles = getMovieTitles();
+
+        // Add HTML for the movies list
+        html = html + "<h1>All Movies</h1>";
+        
+        // Start the HTML list
+        html = html + "<ul>";
+
+        // Finally we can print out all of the movies
+        for (String title : movieTitles) {
+            html = html + "<li>" + title + "</li>";
+        }
+
+        // Finish the List HTML
+        html = html + "</ul>";
+        
+        // Altneratively we can use JDBCConnection to add HTML for the movies list
+        JDBCConnection jdbc = new JDBCConnection();
+        ArrayList<Movie> movies = jdbc.getMovies();
+        html = html + "<h1>All Movies with Years (from JDBCConnection)</h1>" +
+                      "<ul>";
+        for (Movie movie : movies) {
+            html = html + "<li>" + movie.name + " was made in " + movie.year + "</li>";
+        }
+        html = html + "</ul>";
+
+        // TODO: Add your code here for the other queries 
+        int count  = jdbc.countMovies();
+        html = html + "<h1> The number of movies in the database: (from JDBCConnection)</h1>" 
+                    + count;
+        
+        ArrayList<String> bestHorComs = jdbc.bestHorrorComedy();
+        html = html + "<h1> The best movies in the horror and comedy genre are: (from JDBCConnection)</h1>" +
+                      "<ul>";
+        for (String bestHorCom : bestHorComs) {
+            html = html + "<li>" + bestHorCom + "</li>";
+        }
+        html = html + "</ul>";
+
+        ArrayList<String> nuremberg = jdbc.nurembergMovies();
+        html = html + "<h1> The movies with the Nuremberg included in their names are: (from JDBCConnection)</h1>" +
+                      "<ul>";
+        for (String nurem : nuremberg) {
+            html = html + "<li>" + nurem + "</li>";
+        }
+        html = html + "</ul>";
+
+        // Close Content div
+        html = html + "</div>";
+
+        // Footer
+        html = html + """
+            <div class='footer'>
+                <p>COSC3056 Module 0 - Week 05</p>
+            </div>
+        """;
+
+        // Finish the HTML webpage
+        html = html + "</body>" + "</html>";
+
+
+        // DO NOT MODIFY THIS
+        // Makes Javalin render the webpage
+        context.html(html);
     }
 
     /**
-     * Get all of the Movies in the database.
+     * Get all of the Movie Titles in the database
      * @return
-     *    Returns an ArrayList of Movie objects
+     *    Returns an ArrayList of String with ONLY the movie titles
      */
-    public ArrayList<Movie> getMovies() {
-        // Create the ArrayList to return - this time of Movie objects
-        ArrayList<Movie> movies = new ArrayList<Movie>();
+    public ArrayList<String> getMovieTitles() {
+        // Create the ArrayList to return - of Strings for the movie titles
+        ArrayList<String> movies = new ArrayList<String>();
 
         // Setup the variable for the JDBC connection
         Connection connection = null;
 
         try {
             // Connect to JDBC data base
-            connection = DriverManager.getConnection(DATABASE);
+            connection = DriverManager.getConnection(JDBCConnection.DATABASE);
 
             // Prepare a new SQL Query & Set a timeout
             Statement statement = connection.createStatement();
@@ -57,18 +160,13 @@ public class JDBCConnection {
             // The "results" variable is similar to an array
             // We can iterate through all of the database query results
             while (results.next()) {
-                // Create a Movie Object
-                Movie movie = new Movie();
-
-                // Lookup the columns we want, and set the movie object field
+                // We can lookup a column of the a single record in the
+                // result using the column name
                 // BUT, we must be careful of the column type!
-                movie.id    = results.getInt("mvnumb");
-                movie.name  = results.getString("mvtitle");
-                movie.year  = results.getInt("yrmde");
-                movie.genre = results.getString("mvtype");
+                String movieName     = results.getString("mvtitle");
 
-                // Add the movie object to the array
-                movies.add(movie);
+                // Store the movieName in the ArrayList to return
+                movies.add(movieName);
             }
 
             // Close the statement because we are done with it
@@ -88,9 +186,10 @@ public class JDBCConnection {
             }
         }
 
-        // Finally we return all of the movies
+        // Finally we return all of the movie titles
         return movies;
     }
+
 
 
     // TODO: Keep adding more methods here to answer all of the questions from the Studio Class activities
@@ -101,7 +200,7 @@ public class JDBCConnection {
 
         try {
             // Connect to JDBC database
-            connection = DriverManager.getConnection(DATABASE);
+            connection = DriverManager.getConnection(JDBCConnection.DATABASE);
 
             // Prepare a new SQL Query & Set a timeout
             Statement statement = connection.createStatement();
@@ -150,7 +249,7 @@ public ArrayList<String> bestHorrorComedy() {
 
         try {
             // Connect to JDBC database
-            connection = DriverManager.getConnection(DATABASE);
+            connection = DriverManager.getConnection(JDBCConnection.DATABASE);
 
             // Prepare a new SQL Query & Set a timeout
             Statement statement = connection.createStatement();
@@ -200,7 +299,7 @@ public ArrayList<String> nurembergMovies() {
 
         try {
             // Connect to JDBC database
-            connection = DriverManager.getConnection(DATABASE);
+            connection = DriverManager.getConnection(JDBCConnection.DATABASE);
 
             // Prepare a new SQL Query & Set a timeout
             Statement statement = connection.createStatement();
@@ -242,5 +341,4 @@ public ArrayList<String> nurembergMovies() {
         return nuremberg;
     }
 
-    // TODO: Keep adding more methods here to answer all of the questions from the Studio Class activities
 }
